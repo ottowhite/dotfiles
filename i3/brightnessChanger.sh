5f977e5beaeb1f22f3c5f2dc2289c5a2d08752fa
Otto White <whiteotto4@gmail.com>
2023-02-17 18:47:05 +0000

    feat: make brightnessChanger script manipulate backlight

--- i3/brightnessChanger.sh
+++ i3/brightnessChanger.sh
@@ -2,7 +2,22 @@
 
 screenName="eDP-1-1"
 
-brightness=$(xrandr --verbose | grep "Brightness: " | cut -f2 -d " ")
-xrandr --output $screenName --brightness $(($brightness + $1))
-brightness=$(xrandr --verbose | grep "Brightness: " | cut -f2 -d " ")
-notify-send -t 200 "Brightness" $brightness
+max_brightness_raw=$(cat /sys/class/backlight/intel_backlight/max_brightness)
+increment=$(($max_brightness_raw / 50))
+
+# brightness=$(xrandr --verbose | grep "Brightness: " | cut -f2 -d " ")
+
+brightness_raw=$(cat /sys/class/backlight/intel_backlight/brightness)
+brightness=$(($brightness_raw / $max_brightness_raw))
+
+echo $(($brightness_raw + $1 * $increment)) > /sys/class/backlight/intel_backlight/brightness
+
+brightness_raw=$(cat /sys/class/backlight/intel_backlight/brightness)
+
+# notify-send -t 200 "Brightness" $brightness_raw
+
+# xrandr --output $screenName --brightness $(($brightness + $1))
+
+# xrandr --output $screenName --brightness $(($brightness + $1))
+# brightness=$(xrandr --verbose | grep "Brightness: " | cut -f2 -d " ")
+# notify-send -t 200 "Brightness" $brightness
